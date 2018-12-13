434d74d0e08a3d29f7742c00aa31f08b8997f5a9
laurence <laurencehall1@outlook.com>
2018-12-13 16:37:37 -0500

    typo removed

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ class App extends Component {
               />
             }
           />      
-        )}     
+            
         </div>
             </MuiThemeProvider>
       </BrowserRouter>
